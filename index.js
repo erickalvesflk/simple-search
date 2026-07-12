32e2a0b9af4dcb1b2309d2faa2fcc7584014d381
Erick Alves <erickalvesflk@gmail.com>
2026-07-11 22:20:59 -0300

    Iniciando tentativa de função recursiva

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Ideia 
 // fazer um map, onde cada letra que há nas palavras é uma chave de uma array com outras letras que são chaves para outras...
 
-const PRODUCTS = ["T-Shirt","Pants","Shoes","Shirt","Socks","Coat","Jacket"];
+const PRODUCTS = ["T-Shirt","Pants","Shoes","Shirt","Shit","Socks","Coat","Jacket"];
 
 class SearchAlgorithm {
 
@@ -18,24 +18,40 @@ class SearchAlgorithm {
         let finished = false;
 
         /** @returns {Map} */
-        function constructLevel(array){
+        function constructLevel(array, view_index = 0){
             let newMap = new Map();
 
-            items.map(array => {
-                if(!newMap.has(array[0])){
-                    newMap.set(array[0],[]);
+            array.map(element => {
+                if(!newMap.has(element[view_index])){
+                    newMap.set(element[view_index],[]);
                 };
     
-                newMap.get(array[0]).push(array);
+                newMap.get(element[view_index]).push(element);
             });
 
             return newMap;
         }
-        
-        this.#searchMap = constructLevel(items)
-        this.#searchMap.forEach((v,k)=>{
 
-        })
+        function constructOtherLevels(map) {
+            
+        }
+        
+        this.#searchMap = constructLevel(items,0)
+
+        while (!finished) {
+            let all_1_op = true
+            this.#searchMap.forEach((value, key)=>{
+                if ((value.length > 1)){
+                    all_1_op = false;
+                    let n = constructLevel(value)
+                    console.log(n)
+                    this.#searchMap.set(key,constructLevel(value,1));
+                }
+                if (all_1_op){
+                    finished = true;
+                }
+            })
+        }
 
         console.log(this.#searchMap)
     };
