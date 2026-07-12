1b9541f83f1acd5c43f0cd7adcdfe0b09cee25af
Erick Alves <erickalvesflk@gmail.com>
2026-07-11 21:34:41 -0300

    Creating First Level

--- index.js
+++ index.js
@@ -8,11 +8,38 @@ class SearchAlgorithm {
     #searchMap = new Map();
 
     constructor(items) {
+        if(typeof(items) != "object") return;
 
+        this.#buildSearch(items);
     };
 
-    #buildSearch(){
+    /**@param {String[]} items */
+    #buildSearch(items){
+        let finished = false;
+
+        /** @returns {Map} */
+        function constructLevel(array){
+            let newMap = new Map();
+
+            items.map(array => {
+                if(!newMap.has(array[0])){
+                    newMap.set(array[0],[]);
+                };
+    
+                newMap.get(array[0]).push(array);
+            });
+
+            return newMap;
+        }
         
+        this.#searchMap = constructLevel(items)
+        this.#searchMap.forEach((v,k)=>{
+
+        })
+
+        console.log(this.#searchMap)
     };
 
 };
+
+let ClothesSearch = new SearchAlgorithm(PRODUCTS);
