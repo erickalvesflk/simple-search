b9b117b5e7500ef5d6bedefa2925210a41eccb5e
Erick Alves <erickalvesflk@gmail.com>
2026-07-11 21:00:21 -0300

    build start script

---
+++ index.js
@@ -0,0 +1,18 @@
+// Ideia 
+// fazer um map, onde cada letra que há nas palavras é uma chave de uma array com outras letras que são chaves para outras...
+
+const PRODUCTS = ["T-Shirt","Pants","Shoes","Shirt","Socks","Coat","Jacket"];
+
+class SearchAlgorithm {
+
+    #searchMap = new Map();
+
+    constructor(items) {
+
+    };
+
+    #buildSearch(){
+        
+    };
+
+};
