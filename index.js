75acfa12449a0e0b3225030875e681be614f3939
Erick Alves <erickalvesflk@gmail.com>
2026-07-13 17:26:13 -0300

    Resolved the bug of  the undefined box ("socker","soc")

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Ideia 
 // fazer um map, onde cada letra que há nas palavras é uma chave de uma array com outras letras que são chaves para outras...
 
-const PRODUCTS = ["T-Shirt","Pants","Shoes","Shirt","Socker","Coat","Jacket"];
+const PRODUCTS = ["T-Shirt","Pants","Shoes","Shirt","Socker","sockera","Coat","Jacket"];
 
 class SearchAlgorithm {
 
-    #searchMap;
+    searchMap;
 
     /**
      * 
@@ -16,22 +16,39 @@ class SearchAlgorithm {
         if(typeof(items) != "object") return;
         this.devMode = devMode;
         this.items = this.#verifierAndFormater(items);
-        this.#searchMap = this.#buildSearch(this.items);
+        this.searchMap = this.#buildSearch(this.items);
     };
 
     #unpackContainer(container){
+        if(this.devMode) console.log("- - UNPACKING CONTAINER: ",container)
         let unpackedBoxesArray = [];
+        let devModeForLocalFunction = this.devMode
         function unpackBoxes(boxes){
-            if(boxes.size == 1){
-                unpackedBoxesArray.push(boxes.get(0));
-                return;
+            if(devModeForLocalFunction) console.log("- Staring [unpackBoxes]: for the boxes",boxes)
+                if(boxes.size == 1){
+                    if(boxes.has(0)){
+                        unpackedBoxesArray.push(boxes.get(0));
+                        return;
+                    }
+                    // Especial Case, so we need unpack this box with only lenght 1, whose there are 2 boxes inside!
+                    if(devModeForLocalFunction) console.log("- The boxes: ",boxes, "have a special case! Let's unpack that")
             };
             
             for(const box of boxes){
-                if(box[1].has(0)) {
-                    unpackedBoxesArray.push(box[1].get(0))
-                    continue;
+                if(devModeForLocalFunction) console.log("unpacking box",box[1])
+                    if(box[1].has(0)) {
+                        if(devModeForLocalFunction) console.log("- Box:",box[1],"has the box 0, so push",box[1].get(0))
+                            
+                            unpackedBoxesArray.push(box[1].get(0))
+                            continue;
+                    if(box[1].has("")){
+                        // the especial final box is a box with the key letter "" simbolizing the others letters keys form the item
+                        if(devModeForLocalFunction) console.log("- Box:",box[1],"is a especial, so push",box[1].get(""))
+                            let finalBox = box[1].get("")
+                        unpackedBoxesArray.push(finalBox.get(0))
+                    }
                 }else{
+                    if(devModeForLocalFunction) console.log("- Box:",box[1],"need be unpacked")
                     unpackBoxes(box[1]);
                 }
             }
@@ -43,10 +60,12 @@ class SearchAlgorithm {
     /**
      * 
      * @param {string} item - item witch will be search
+     * @returns {string[] | []}
      */
     search(item){
+        if (this.devMode) console.log("- STARTING THE SEARCH")
         let formatedItem = item.toLocaleLowerCase();
-        let actualContainer = this.#searchMap;
+        let actualContainer = this.searchMap;
 
         for(const letter of formatedItem){
             if(actualContainer.has(letter)){
@@ -55,9 +74,14 @@ class SearchAlgorithm {
                     // há uma resposta
                     return [actualContainer.get(0)];
                 };
-
+                if(actualContainer.has("")){
+                    // the especial final box is a box with the key letter "" simbolizing the others letters keys form the item
+                    let finalBox = actualContainer.get("")
+                    return [finalBox.get(0)]
+                }    
             };
         };
+        if (this.devMode) console.log("the final actual container is: ",actualContainer);
 
         let result = this.#unpackContainer(actualContainer)
         return result;
@@ -70,6 +94,7 @@ class SearchAlgorithm {
     #verifierAndFormater(items){
         let HashMap = new Map();
         let formatedItems = []
+
         for(const item of items){
             formatedItems.push(item.toLocaleLowerCase())
             if(!HashMap.has(item)){
@@ -95,6 +120,14 @@ class SearchAlgorithm {
         
         elements.map(element => {
             let keyLetter = element[letter_pos];
+            if (!keyLetter) {
+                // undefined key, so there are one option.
+                console.log(keyLetter,"is ouch of the limit in the position",letter_pos,"of the string",element)
+                let newFinalBox = new Map()
+                newFinalBox.set(0,element)
+                newContainer.set(" ",newFinalBox)
+                return;
+            }
             
             if(!newContainer.has(keyLetter)){
                 let newBox = new Map();
@@ -165,4 +198,5 @@ class SearchAlgorithm {
 };
 
 let ClothesSearch = new SearchAlgorithm(PRODUCTS);
-console.log(ClothesSearch.search(""))
+console.log(ClothesSearch.searchMap)
+console.log(ClothesSearch.search("sockera"))
